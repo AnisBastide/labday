52ebf51860a12fa5be9de3e29a0ff00833af2871
anis bastide <anis.bastide@gmail.com>
2020-04-23 17:40:21 +0200

    started to move a character

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,41 @@ const gameConfig: Phaser.Types.Core.GameConfig = {
   },
 
   parent: 'game',
-  backgroundColor: '#000000',
+  scene: {
+    preload,
+    create,
+    update,
+  }
 };
+const gameState={
+  cursors: undefined,
+  codey: undefined
+};
+function preload() {
+  this.load.image('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/codey.png');
+}
+
+function create() {
+  gameState.codey = this.add.sprite(150, 200, 'codey');
+  gameState.cursors=this.input.keyboard.createCursorKeys();
+  // Set cursor keys here!
+}
+
+function update() {
+  // Update based on keypress here!
+  if(gameState.cursors.right.isDown){
+    gameState.codey.x+=5;
+  }
+  if(gameState.cursors.left.isDown){
+    gameState.codey.x-=5;
+  }
+  if(gameState.cursors.up.isDown){
+    gameState.codey.y-=5;
+    setTimeout(function(){gameState.codey.y+=5},100);
+  }
+  if(gameState.cursors.down.isDown){
+    gameState.codey.y+=5;
+  }
+}
 
 export const game = new Phaser.Game(gameConfig);
