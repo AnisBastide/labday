d41c884a5d5da69c0eb2fc23eed61ae6926f827b
anis bastide <anis.bastide@upslide.net>
2020-05-13 10:52:25 +0200

    added platforms,physics and collider

--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,69 @@
 import * as Phaser from 'phaser';
 
 const gameConfig: Phaser.Types.Core.GameConfig = {
-  title: 'Sample',
+    title: 'Sample',
 
-  type: Phaser.AUTO,
+    type: Phaser.AUTO,
 
-  scale: {
-    width: window.innerWidth,
-    height: window.innerHeight,
-  },
-
-  physics: {
-    default: 'arcade',
-    arcade: {
-      debug: true,
+    scale: {
+        width: window.innerWidth,
+        height: window.innerHeight,
     },
-  },
 
-  parent: 'game',
-  scene: {
-    preload,
-    create,
-    update,
-  }
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 800},
+            debug: false,
+        },
+    },
+    parent: 'game',
+    scene: {
+        preload,
+        create,
+        update,
+    },
 };
-const gameState={
-  cursors: undefined,
-  codey: undefined
+const gameState = {
+    cursors: undefined,
+    player: undefined,
 };
+
 function preload() {
-  this.load.image('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/codey.png');
+    this.load.image('player', 'assets/Character1.png');
+    this.load.image('ground', 'assets/Platform2.png');
+    this.load.image('wall','assets/wall.png');
 }
 
 function create() {
-  gameState.codey = this.add.sprite(150, 200, 'codey');
-  gameState.cursors=this.input.keyboard.createCursorKeys();
-  // Set cursor keys here!
+    // gameState.codey = this.add.sprite(150, 200, 'codey');
+    //  gameState.cursors = this.input.keyboard.createCursorKeys();
+    // Set cursor keys here!
+    var platforms = this.physics.add.staticGroup();
+    platforms.create(48, innerHeight - 12, 'ground');
+    platforms.create(96+12, innerHeight - 48, 'wall');
+    gameState.player = this.physics.add.sprite(48, innerHeight -100, 'player');
+    gameState.player.setBounce(0.2);
+    gameState.player.setCollideWorldBounds(true);
+    this.player = gameState.player;
+    this.physics.add.collider(gameState.player, platforms);
+    gameState.cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
-  // Update based on keypress here!
-  if(gameState.cursors.right.isDown){
-    gameState.codey.x+=5;
-  }
-  if(gameState.cursors.left.isDown){
-    gameState.codey.x-=5;
-  }
-  if(gameState.cursors.up.isDown){
-    gameState.codey.y-=5;
-  }
-  if(gameState.cursors.down.isDown){
-    gameState.codey.y+=5;
-  }
-  if(gameState.cursors.space.isDown){
-    gameState.codey.y-=5;
-    setTimeout(function(){gameState.codey.y+=5},100);
+    // Update based on keypress here!
+    if (gameState.cursors.left.isDown) {
+        gameState.player.setVelocityX(-160);
 
-  }
+    } else if (gameState.cursors.right.isDown) {
+        gameState.player.setVelocityX(160);
+
+    } else {
+        this.player.setVelocityX(0);
+    }
+    if (gameState.cursors.up.isDown && gameState.player.body.touching.down) {
+        gameState.player.setVelocityY(-330);
+    }
 }
 
 export const game = new Phaser.Game(gameConfig);
