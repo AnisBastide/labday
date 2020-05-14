248dda4ade4565720ed9c7dde9f0b3346446aeb3
anis bastide <anis.bastide@upslide.net>
2020-05-14 10:01:04 +0200

    added some platforms and the idle animation of the character

--- src/main.ts
+++ src/main.ts
@@ -30,9 +30,11 @@ const gameState = {
 };
 
 function preload() {
-    this.load.image('player', 'assets/Character1.png');
-    this.load.image('ground', 'assets/Platform2.png');
-    this.load.image('wall','assets/wall.png');
+    this.load.spritesheet('player', 'assets/Idle_Character.png', {frameWidth: 32, frameHeight: 64});
+    this.load.image('ground', 'assets/ground.png');
+    this.load.image('platform', 'assets/platform.png');
+    this.load.image('dirt', 'assets/blocdirt.png');
+    this.load.image('smallplatform', 'assets/smallplatform.png');
 }
 
 function create() {
@@ -40,26 +42,72 @@ function create() {
     //  gameState.cursors = this.input.keyboard.createCursorKeys();
     // Set cursor keys here!
     var platforms = this.physics.add.staticGroup();
-    platforms.create(48, innerHeight - 12, 'ground');
-    platforms.create(108, innerHeight - 48, 'wall');
-    gameState.player = this.physics.add.sprite(48, innerHeight -100, 'player');
+    platforms.create(920, innerHeight - 12, 'ground');
+    platforms.create(1840, innerHeight - 12, 'ground');
+    platforms.create(150, innerHeight - 100, 'platform');
+    // first floor
+    platforms.create(332, innerHeight - 57, 'smallplatform');
+    platforms.create(364, innerHeight - 57, 'dirt');
+    platforms.create(388, innerHeight - 57, 'dirt');
+    platforms.create(388, innerHeight - 57, 'dirt');
+    platforms.create(412, innerHeight - 57, 'dirt');
+    platforms.create(436, innerHeight - 57, 'dirt');
+    platforms.create(460, innerHeight - 57, 'dirt');
+    platforms.create(484, innerHeight - 57, 'smallplatform');
+    // second floor
+    platforms.create(356, innerHeight - 81, 'smallplatform');
+    platforms.create(388, innerHeight - 81, 'dirt');
+    platforms.create(388, innerHeight - 81, 'dirt');
+    platforms.create(412, innerHeight - 81, 'dirt');
+    platforms.create(436, innerHeight - 81, 'dirt');
+    platforms.create(460, innerHeight - 81, 'smallplatform');
+    // third floor
+    platforms.create(380, innerHeight - 105, 'smallplatform');
+    platforms.create(388, innerHeight - 105, 'dirt');
+    platforms.create(412, innerHeight - 105, 'dirt');
+    platforms.create(460, innerHeight - 105, 'smallplatform');
+    gameState.player = this.physics.add.sprite(48, innerHeight - 100, 'player');
     gameState.player.setBounce(0.2);
     gameState.player.setCollideWorldBounds(true);
     this.player = gameState.player;
     this.physics.add.collider(gameState.player, platforms);
     gameState.cursors = this.input.keyboard.createCursorKeys();
+    
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('player', {start: 0, end: 1}),
+        frameRate: 10,
+        repeat: -1,
+    });
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('player', {start: 0, end: 1}),
+        frameRate: 10,
+        repeat: -1,
+    });
+    this.anims.create({
+        key: 'idle',
+        frames: this.anims.generateFrameNumbers('player', {start: 0, end: 1}),
+        frameRate: 1,
+        repeat: -1,
+    });
 }
 
 function update() {
     // Update based on keypress here!
     if (gameState.cursors.left.isDown) {
         gameState.player.setVelocityX(-160);
-
+        // gameState.player.anims.play('left', true);
+        gameState.player.anims.play('idle', false);
     } else if (gameState.cursors.right.isDown) {
         gameState.player.setVelocityX(160);
-
+        // gameState.player.anims.play('right', true);
+        gameState.player.anims.play('idle', false);
     } else {
         this.player.setVelocityX(0);
+        // gameState.player.anims.play('right', false);
+        // gameState.player.anims.play('left', false);
+        gameState.player.anims.play('idle', true);
     }
     if (gameState.cursors.up.isDown && gameState.player.body.touching.down) {
         gameState.player.setVelocityY(-330);
