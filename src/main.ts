7d8e728a4882144194eb4901d9062bb9281a3f8c
anis bastide <anis.bastide@upslide.net>
2020-05-15 15:13:02 +0200

    added animation for punch and jump (not working properly but in progress)

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ const gameState = {
 };
 
 function preload() {
-    this.load.spritesheet('player', 'assets/Idle_Character.png', {frameWidth: 32, frameHeight: 64});
+    this.load.spritesheet('player', 'assets/spritesheetExample.png', {frameWidth: 144, frameHeight: 144});
     this.load.image('ground', 'assets/ground.png');
     this.load.image('platform', 'assets/platform.png');
     this.load.image('dirt', 'assets/blocdirt.png');
@@ -41,10 +41,10 @@ function create() {
     // gameState.codey = this.add.sprite(150, 200, 'codey');
     //  gameState.cursors = this.input.keyboard.createCursorKeys();
     // Set cursor keys here!
-    var platforms = this.physics.add.staticGroup();
+    const platforms = this.physics.add.staticGroup();
     platforms.create(920, innerHeight - 12, 'ground');
     platforms.create(1840, innerHeight - 12, 'ground');
-    platforms.create(150, innerHeight - 100, 'platform');
+    platforms.create(150, innerHeight - 300, 'platform');
     // first floor
     platforms.create(332, innerHeight - 57, 'smallplatform');
     platforms.create(364, innerHeight - 57, 'dirt');
@@ -66,28 +66,48 @@ function create() {
     platforms.create(388, innerHeight - 105, 'dirt');
     platforms.create(412, innerHeight - 105, 'dirt');
     platforms.create(460, innerHeight - 105, 'smallplatform');
-    gameState.player = this.physics.add.sprite(48, innerHeight - 100, 'player');
+    //
+    //
+    gameState.player = this.physics.add.sprite(1500, innerHeight - 150, 'player');
     gameState.player.setBounce(0.2);
-    gameState.player.setCollideWorldBounds(true);
     this.player = gameState.player;
     this.physics.add.collider(gameState.player, platforms);
     gameState.cursors = this.input.keyboard.createCursorKeys();
-    
+    this.cameras.main.setBounds(0, 0, 5000, 5000);
+    this.cameras.main.startFollow(this.player);
     this.anims.create({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('player', {start: 0, end: 1}),
+        frames: this.anims.generateFrameNumbers('player', {start: 12, end: 17}),
         frameRate: 10,
         repeat: -1,
     });
     this.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('player', {start: 0, end: 1}),
-        frameRate: 10,
+        frames: this.anims.generateFrameNumbers('player', {start: 12, end: 17}),
+        frameRate: 5,
         repeat: -1,
     });
     this.anims.create({
         key: 'idle',
-        frames: this.anims.generateFrameNumbers('player', {start: 0, end: 1}),
+        frames: this.anims.generateFrameNumbers('player', {start: 4, end: 1}),
+        frameRate: 5,
+        repeat: -1,
+    });
+    this.anims.create({
+        key: 'punch',
+        frames: this.anims.generateFrameNumbers('player', {start: 18, end: 22}),
+        frameRate: 10,
+        repeat: -1,
+    });
+    this.anims.create({
+        key: 'jump',
+        frames: this.anims.generateFrameNumbers('player', {start: 6, end: 6}),
+        frameRate: 1,
+        repeat: -1,
+    });
+    this.anims.create({
+        key: 'fall',
+        frames: this.anims.generateFrameNumbers('player', {start: 7, end: 10}),
         frameRate: 1,
         repeat: -1,
     });
@@ -97,20 +117,29 @@ function update() {
     // Update based on keypress here!
     if (gameState.cursors.left.isDown) {
         gameState.player.setVelocityX(-160);
-        // gameState.player.anims.play('left', true);
-        gameState.player.anims.play('idle', false);
+        if (!(gameState.player.anims.getCurrentKey() === 'left')) {
+            gameState.player.anims.play('left', true);
+        }
     } else if (gameState.cursors.right.isDown) {
         gameState.player.setVelocityX(160);
-        // gameState.player.anims.play('right', true);
-        gameState.player.anims.play('idle', false);
+        if (!(gameState.player.anims.getCurrentKey() === 'right')) {
+            gameState.player.anims.play('right', true);
+        }
+    } else if (gameState.cursors.space.isDown) {
+        if (!(gameState.player.anims.getCurrentKey() === 'punch')) {
+            gameState.player.anims.play('punch', true);
+        }
+
     } else {
         this.player.setVelocityX(0);
-        // gameState.player.anims.play('right', false);
-        // gameState.player.anims.play('left', false);
         gameState.player.anims.play('idle', true);
     }
-    if (gameState.cursors.up.isDown && gameState.player.body.touching.down) {
+    if (gameState.cursors.up.isDown && gameState.player.body.touching.down && !(gameState.player.anims.getCurrentKey() === 'jump')) {
         gameState.player.setVelocityY(-330);
+        gameState.player.anims.play('jump', true);
+    }
+    if ( !gameState.player.body.touching.down && !(gameState.player.anims.getCurrentKey() === 'fall')) {
+        gameState.player.anims.play('fall', true);
     }
 }
 
